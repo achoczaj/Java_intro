f095cb23af4c53b867892f45f92010abb225f783
Arkadiusz Choczaj <arkadiusz.choczaj@gmail.com>
2017-04-25 16:41:18 +0100

    initial commit

---
+++ PrintMotto.java
@@ -0,0 +1,9 @@
+package com.myJavaIntro;
+
+class PrintMotto {
+    public static void main(String[] arguments) {
+
+        String motto = "Veritas et libertas!";
+        System.out.println(motto);
+    }
+}
